7d3eb7a8a7a797bb8e8579c2e232d34e6fd33b95
unknown <LucasFeliciano02@outlook.com>
2022-04-02 09:07:38 -0300

    Significados dos principais termos sql

---
+++ mysql_DBeaver/Termos_e_Siglas.sql
@@ -0,0 +1,54 @@
+
+
+• DICIONARIO SQL: SIGNIFICADOS DOS PRINCIPAIS TERMOS E SIGLAS
+
+
+- SQL = Structured Query Language(Linguagem de programação para consulta de dados)
+
+- BD or DB = database, um banco de dados é um lugar onde armazenamos dados/informações, dentro das tabelas
+
+- MER = (Modelo Entidade Relacionamento) é um modelo que descreve como cada entidade, tabela de uma banco de dados vai se relacionar com a outra
+  ou seja, como uma entidade Produto se relaciona com uma outra entidade Venda  
+  
+- DER = (Diagrama Entidade Relacionamento) é uma representação gráfica do MER
+  
+- SGBD = (Sistema de Gerenciamento de Banco de Dados). é um programa que vamos usar para acessar, guardar, criar e gerenciar os Bancos de Dados, por meio da linguagem SQL
+
+- MySQL, SQLServer, PostgreeSQL, SQLite, Oracle = São SGBDs e sao os nomes dos diferentes pprogramas de banco de dados (SGBDs) ONDE PPODEMOS USAR O SQL  
+
+- DBA = (database Administrator). Se trata de uma profissao que trabalham com banco de dados e SQL  
+
+- schema = É outra forma de se referir a BD (Banco de dados) em alguns programas de banco de dados 
+
+- CRUD = Conjunto de comandos realizados dentro de banco de dados para:
+  
+  - Criar(Create) dados
+  - Ler(read) dados
+  - Atualizar(Update) dados
+  - Deletar(Delete) dados
+
+- ACID = Atomicidade, Consistência, Isolamento e Durabilidade. São propriedades das transações em banco de dados (como ISNERT, update E DELETE)
+
+
+• SQL Commands:
+
+
+- DDL(data Definition Language): Linguagem de definição de dados é usada para cirar e modificar a estrutura dos objetos armazenados em banco de dados:
+Ex: create, alter, drop, rename, truncate, commit
+
+- DQL(data Query Language): Linguagem de consulta de dados são os comandos de consulta aos dados armazenados em banco de dados:
+Ex: select (Em alguns lugares esse comando pode ser agruados no DML)
+
+- DML(data Manipulation Language): Linguagem de manipulação de Dados é um conunto de comandos para manipulação dos dados dentro das tabelas em banco de dados:
+Ex: insert, udate, delete, merge, call, explain plan, lock table
+
+- DCL(data Control Language): Linguagem de Controde de Dados é usada para controle de acesso e permissões dos usuários em um banco de dados:
+Ex: grant, revoke E DENY
+
+- TCL(transaction Control Language): Linguagem de controle de transações são comandos usados para gerenciar as mudanças feitas pelos comandos DML
+Ex: commit, rollback E savepoint.
+
+
+
+
+
